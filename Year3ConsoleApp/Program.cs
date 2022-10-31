23d72a3edb33c0c17fc3a002bf2a2d3be3ffc049
Gaurav K Nepal <gauravnepal3@gmail.com>
2022-10-31 10:10:32 +0545

    Inital Commit

--- Year3ConsoleApp/Program.cs
+++ Year3ConsoleApp/Program.cs
@@ -3,68 +3,84 @@
 using Year3ConsoleApp.Basics;
 using Year3ConsoleApp.Testing;
 
-
-//getting input from users
-// Declare variables and then initialize to zero.
-int num1 = 0; int num2 = 0;
-
-// Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-// Ask the user to type the first number.
-Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToInt32(Console.ReadLine());
-
-// Ask the user to type the second number.
-Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToInt32(Console.ReadLine());
-
-// Ask the user to choose an option.
-Console.WriteLine("Choose an option from the following list:");
-Console.WriteLine("Press 1 for Addition");
-Console.WriteLine("Press 2 for Subtraction");
-Console.WriteLine("Press 3 for Multiplication");
-Console.WriteLine("Press 4 for Division");
-Console.WriteLine("Press 5 for Jagged Arrays");
-Console.WriteLine("Press 6 for SingleDimensional Arrays");
-Console.WriteLine("Press 7 for ImplicitlyTyped Arrays");
-Console.WriteLine("Press 8 for MultiDimensional Arrays");
-Console.Write("Your option? ");
+using Year3ConsoleApp.Week2;
 
 
-// Use a switch statement to do the math.
-switch (Console.ReadLine())
-{
-    case "1":
-        Calculator.Additions(num1, num2);
-        break;
-    case "2":
-        Calculator.Subtraction(num1, num2);
-        break;
-    case "3":
-        Calculator.Multiplication(num1, num2);
-        break;
-    case "4":
-        Calculator.Division(num1, num2);
-        break;
-    case "5":
-        Arrays.JaggedArray();
-        break;
-    case "6":
-        Arrays.SingleDimensionalArray();
-        break;
-    case "7":
-        Arrays.ImplicitlyTypedArray();
-        break;
-    case "8":
-        Arrays.MultiDimensionalArray();
-        break;
-    default:
-        Interpolation.StringInterpolation();
-        break;
-}
+////getting input from users
+//// Declare variables and then initialize to zero.
+//int num1 = 0; int num2 = 0;
 
-// Wait for the user to respond before closing.
-Console.Write("Press any key to close the Calculator console app...");
-Console.ReadKey();
+//// Display title as the C# console calculator app.
+//Console.WriteLine("Console Calculator in C#\r");
+//Console.WriteLine("------------------------\n");
+
+//// Ask the user to type the first number.
+//Console.WriteLine("Type a number, and then press Enter");
+//num1 = Convert.ToInt32(Console.ReadLine());
+
+//// Ask the user to type the second number.
+//Console.WriteLine("Type another number, and then press Enter");
+//num2 = Convert.ToInt32(Console.ReadLine());
+
+//// Ask the user to choose an option.
+//Console.WriteLine("Choose an option from the following list:");
+//Console.WriteLine("Press 1 for Addition");
+//Console.WriteLine("Press 2 for Subtraction");
+//Console.WriteLine("Press 3 for Multiplication");
+//Console.WriteLine("Press 4 for Division");
+//Console.WriteLine("Press 5 for Jagged Arrays");
+//Console.WriteLine("Press 6 for SingleDimensional Arrays");
+//Console.WriteLine("Press 7 for ImplicitlyTyped Arrays");
+//Console.WriteLine("Press 8 for MultiDimensional Arrays");
+//Console.Write("Your option? ");
+
+
+//// Use a switch statement to do the math.
+//switch (Console.ReadLine())
+//{
+//    case "1":
+//        Calculator.Additions(num1, num2);
+//        break;
+//    case "2":
+//        Calculator.Subtraction(num1, num2);
+//        break;
+//    case "3":
+//        Calculator.Multiplication(num1, num2);
+//        break;
+//    case "4":
+//        Calculator.Division(num1, num2);
+//        break;
+//    case "5":
+//        Arrays.JaggedArray();
+//        break;
+//    case "6":
+//        Arrays.SingleDimensionalArray();
+//        break;
+//    case "7":
+//        Arrays.ImplicitlyTypedArray();
+//        break;
+//    case "8":
+//        Arrays.MultiDimensionalArray();
+//        break;
+//    default:
+//        Interpolation.StringInterpolation();
+//        break;
+//}
+
+//// Wait for the user to respond before closing.
+//Console.Write("Press any key to close the Calculator console app...");
+//Console.ReadKey();
+
+
+
+//Console.WriteLine("Enter a first number");
+//var a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter a second number");
+//var b = Convert.ToInt32(Console.ReadLine());
+//ControlStatemnet.Greater(a, b);
+
+
+//ControlStatemnet.SwitchCase();
+//ControlStatemnet.DoWhileStatement();
+ControlStatemnet.ForEachStatemnet();
+
---
+++ Year3ConsoleApp/Week2/ControlStatemnet.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Year3ConsoleApp.Week2
+{
+    public class ControlStatemnet
+    {
+        public static void Greater(int a,int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine(a + " is greater than " + b);
+            }
+            else if (b > a)
+            {
+                Console.WriteLine(b + " is greater than " + a);
+            }
+            else
+            {
+                Console.WriteLine(a + " and " + b + " are equal");
+
+            }
+            Console.ReadLine();
+        }
+
+        public static void SwitchCase()
+        {
+            Console.WriteLine("Press 1 for display number");
+            Console.WriteLine("Press 2 for display string");
+            Console.WriteLine("Enter your option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("1,2,3,4,5,6");
+                    break;
+                case "2":
+                    Console.WriteLine("a,b,c,d,e,f");
+                    break;
+                default:
+                    Console.WriteLine("No code for this option");
+                        break;
+            }
+            Console.ReadLine();
+        }
+
+        public static void WhileStatement()
+        {
+            int a = 1;
+            while (a < 10)
+            {
+                Console.WriteLine("Value of a is " + a);
+            }
+         }
+        public static void DoWhileStatement()
+        {
+            int a = 0;
+            do
+            {
+                Console.WriteLine("Enter a number");
+                 a = Convert.ToInt32(Console.ReadLine());
+            } while (a < 10);
+        }
+        public static void ForStatement()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void ForEachStatemnet()
+        {
+            //int[] arr_array = new int[] {0, 1, 2, 3, 4, 5 };
+            string[] arr_array = new string[] {"gaurav", "1", "2", "3", "4", "5" };
+            foreach (string i in arr_array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
